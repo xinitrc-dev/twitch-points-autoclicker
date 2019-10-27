08df82cfed5ff371c74fce6108aef4f0ca4a19e6
xinitrc <legiostels@gmail.com>
2019-10-27 14:23:17 -0400

    Initial Lite version

--- src/twitch-clicker.js
+++ src/twitch-clicker.js
@@ -1,5 +1,3 @@
-console.log('Twitch Points Autoclicker: Initialized!');
-
 // Finds the right element, clicks the bonus button
 function clickPoints() {
 	try {
@@ -9,16 +7,10 @@ function clickPoints() {
     catch(err) {}
 }
 
+console.log('Twitch Points Autoclicker: Initialized!');
+
 // Pre-check
 clickPoints();
-
-//Answer background.js handshake
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-	console.log(msg.text);
-    if (msg.text === 'check') {
-      sendResponse({status: 'confirmed'});
-    }
-});
 
 // React to creation of an element with the clicking points script
 document.arrive('.tw-button', clickPoints);
