fe5a33e75b2377c00f747d80457d49bfb9b8b799
xinitrc <contact@xinitrc.ca>
2026-06-26 15:43:38 -0400

    LITE v1.4 Release
    * Updated the bonus chest detection due to UI changes
    * Updated manifest to v3 and added upcoming Mozilla requirements

--- src/twitch-clicker.js
+++ src/twitch-clicker.js
@@ -1,46 +1,31 @@
+function getClaimButton() {
+	return document.querySelector('.claimable-bonus__icon') && document.querySelector('.claimable-bonus__icon').closest('button');
+}
+
 // Finds the right element, clicks the bonus button
 function clickPoints() {
-	try {
-        // Get all clickable buttons inside 'community-points-summary'
-		let elems = document.querySelector('.community-points-summary').querySelectorAll('button');
-		
-		// Click each button, except for the first, which is the points spending menu
-		elems.forEach(function(currentElem, index, arr) {
-			if (index != 0) {
-				// Click the button and display the console log
-				console.log('Twitch Points Autoclicker: Clicked!');
-				currentElem.click();
-			}
-		});
-    }
-    catch(err) {}
-	
+	let button = getClaimButton();
+	if (button) {
+		console.log('Twitch Points Autoclicker: Clicked!');
+		button.click();
+	}
 }
 
 setTimeout(function() {
 	console.log('Twitch Points Autoclicker: Initialized!');
-	const ATTEMPT_NUM = 10;
 
-	(function loopie(i) {
-		setTimeout(function() {
+	// Pre-check
+	let try_button = getClaimButton();
+	if (try_button) {
+		console.log('Twitch Points Autoclicker: Found Button!');
 
-			console.log('Twitch Points Autoclicker: Attempt ' + (11 - i) + '/' + ATTEMPT_NUM);
+		clickPoints();
+	}
 
-			if (document.contains(document.getElementsByClassName('community-points-summary')[0])) {
-				console.log('Twitch Points Autoclicker: Success');
-
-				// Pre-check
-				clickPoints();
-
-				// React to creation of an element with the clicking points script
-				document.getElementsByClassName('community-points-summary').arrive('button', clickPoints);
-
-				i=1; // equivalent of break
-
-			}
-
-			if (--i) loopie(i);   //  decrement i and call myLoop again if i > 0
-		}, 10000)
-	})(ATTEMPT_NUM);
-
+	// React to creation of an element with the clicking points script
+	document.arrive('.claimable-bonus__icon', function() {
+		console.log('Twitch Points Autoclicker: Found Button!');
+		clickPoints();
+	});
+	
 }, 10000);
